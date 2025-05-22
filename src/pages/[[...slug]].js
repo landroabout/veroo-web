69a734b4c7425b49605284d20e444e4ee78626e6
Yom Djo <joelhounak@gmail.com>
2025-05-22 13:08:50 +0000

    Update [[...slug]].js

--- src/pages/[[...slug]].js
+++ src/pages/[[...slug]].js
@@ -49,17 +49,8 @@ export async function getStaticProps({ params }) {
     const data = allContent();
     const urlPath = '/' + (params.slug || []).join('/');
     const props = await resolveStaticProps(urlPath, data);
-   // return { props };
-    return {
-  props: {
-    page: {
-      __metadata: {
-        // modelName: data.modelName || null, // Use null instead of undefined
-        ...(data.modelName !== undefined ? { modelName: data.modelName } : {})
-      }
-    }
-  }
-};
+    return { props };
+
 }
 
 export default Page;
