7a717ff25b000034a87e857b81f1097390db356f
Yom Djo <joelhounak@gmail.com>
2025-06-11 14:32:25 +0000

    Update reindex.js

--- src/pages/api/reindex.js
+++ src/pages/api/reindex.js
@@ -1,6 +1,9 @@
 import { index } from '../../utils/indexer';
 
 export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', 'https://app.google.com');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     try {
         const result = await index();
         res.status(200).send({ result });
